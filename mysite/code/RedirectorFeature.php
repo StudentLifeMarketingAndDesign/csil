0ca120b9d74b1644b12e50d0a980a8488c7fa4b3
Dustin Quam <quamsta@gmail.com>
2013-05-23 13:36:14 -0500

    some fixes for feature pages, added redirector page type

---
+++ mysite/code/RedirectorFeature.php
@@ -0,0 +1,94 @@
+<?php
+class RedirectorFeature extends RedirectorPage{
+
+	public static $db = array(
+	);
+
+	public static $has_one = array(
+	);
+
+
+
+	public function getCMSFields(){
+		$f = parent::getCMSFields();
+
+
+		$f->addFieldToTab("Root.Main", new HTMLEditorField("Content", "Content Preview"));
+
+
+		return $f;
+	}
+
+
+  public function LinkShortName(){
+
+    if($this->ExternalURL){
+
+      $url = $this->ExternalURL;
+
+      $parsed_url = parse_url($url);
+      $host = $parsed_url['host'];
+
+
+      if($host){
+        return $host;
+      }else{
+        return false;
+      }
+
+
+
+
+
+
+    }
+
+
+
+
+  }
+
+
+	/*public function getAllSidebarItems(){
+
+		$items = array();
+
+		$items[] = $this->SidebarItems()->toArray();
+		if(!$this->getParent()){
+
+			return $items;
+
+		}else if($this->InheritSidebarItems){
+
+					}
+
+		return $items;
+
+	}*/
+
+}
+class RedirectorFeature_Controller extends RedirectorPage_Controller {
+
+	/**
+	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+	 * permissions or conditions required to allow the user to access it.
+	 *
+	 * <code>
+	 * array (
+	 *     'action', // anyone can access this action
+	 *     'action' => true, // same as above
+	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+	 * );
+	 * </code>
+	 *
+	 * @var array
+	 */
+
+
+	public function init() {
+		parent::init();
+
+	}
+
+}
--- themes/csil/templates/Layout/FeaturePage.ss
+++ themes/csil/templates/Layout/FeaturePage.ss
@@ -6,23 +6,29 @@
         </div>
     </div>
 
-
- <div class="content-bg">
-  <section class="container content-wrapper feature-page clearfix">
+  <% if $Content %>
+  <div class="feature-content-bg">
+  <section class="container feature-page content-wrapper clearfix">
       <section class="main-content">
         $Form
         $Content
       </section>
+  </section>
+  </div>
+  <% end_if %>
 
-
-       <ul class="features">
+ <div class="content-bg">
+   <section class="container feature-wrapper feature-page clearfix">
+      <ul class="features">
          <% loop Children %>
            <li onclick="javascript:window.location.href='$Link';">
-
+             <div class="feature-image">
+               <img src="http://localhost:8888/csil/assets/Uploads/_resampled/croppedimage350197-home-outreach2.jpg" />
+             </div>
              <div class="feature-content">
-               <h3><a href="$Link">$Title</a></h3>
-               <p>Some placeholder text</p>
-               <span class="read-more"><a href="$Link">More</a></span>
+               <h3><a href="$Link" <% if $RedirectionType = "External" %>class="external" target="_blank"<% end_if %>>$Title</a></h3>
+               <p>$Content.Summary(20)</p>
+               <span class="read-more"><a href="$Link" class="$ClassName" <% if $RedirectionType = "External" %> target="_blank"class="external"<% end_if %>>More <%if LinkShortName %>at $LinkShortName &raquo; <% end_if %></a></span>
                <div class="clearfix"></div>
              </div>
 
