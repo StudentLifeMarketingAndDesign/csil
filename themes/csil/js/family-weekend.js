1cbff3af8fa2fa5f005b1a92ee97b3cee2d3fd84
Dustin Quam <quamsta@gmail.com>
2014-07-01 08:52:32 -0500

    minor affix offset tweaks

--- themes/csil/js/family-weekend.js
+++ themes/csil/js/family-weekend.js
@@ -2,11 +2,7 @@ $(document).ready(function() {
 
 	$('#floaty-nav').affix({
 	  offset: {
-	    top: function() {
-	    	//console.log($('#floaty-nav').scrollTop());
-	    	//return ($('#floaty-nav').scrollTop());
-	    	return($('.fw-content').offset().top);
-	    },
+	    top: 190,
 	    bottom: function() {
 	    	return (this.bottom = $('.footer').outerHeight(true))
 	    }
@@ -15,10 +11,7 @@ $(document).ready(function() {
 	
 	$('#btt').affix({
 	  offset: {
-	    top: 90,
-	    bottom: function() {
-	    	return (this.bottom = $('.footer').outerHeight(true))
-	    }
+	    top: 90
 	  }
 	});
 	
