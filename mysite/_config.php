063161f1c5b5e5c6afe1474525f17a8a3a39aeee
Dustin Quam <dustinquam@me.com>
2014-12-09 09:36:02 -0600

    setadmin email in config.php

--- mysite/_config.php
+++ mysite/_config.php
@@ -13,7 +13,7 @@ require_once("conf/ConfigureFromEnv.php");
 // Set the current theme. More themes can be downloaded from
 // http://www.silverstripe.org/themes/
 SSViewer::set_theme('simple');
-
+Email::setAdminEmail('imu-web@uiowa.edu');
 // Set the site locale
 i18n::set_locale('en_US');
 FulltextSearchable::enable();
