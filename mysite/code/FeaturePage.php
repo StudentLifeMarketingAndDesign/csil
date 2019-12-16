5870d01184280291c4f4c87239305eeed2c58152
Dustin Quam <quamsta@gmail.com>
2019-12-16 10:55:51 -0600

    updated gitignore, added featured staff heading field

--- mysite/code/FeaturePage.php
+++ mysite/code/FeaturePage.php
@@ -1,15 +1,19 @@
 <?php
 
 use SilverStripe\Forms\CheckboxSetField;
+use SilverStripe\Forms\TextField;
 class FeaturePage extends Page {
 
 	private static $db = array(
-
+		'FeaturedStaffHeading' => 'Varchar(255)'
 	);
 
 	private static $has_one = array(
-	
 
+	);
+
+	private static $defaults = array(
+		'FeaturedStaffHeading' => 'Related staff'
 	);
 	private static $belongs_many_many = array (
 		"StaffPages" => "StaffPage",
@@ -24,7 +28,7 @@ class FeaturePage extends Page {
 		$f = parent::getCMSFields();
 
 
-		
+		$f->addFieldToTab("Root.Main", new TextField("FeaturedStaffHeading", 'Featured Staff Heading (default: Related staff)'));
 		$f->addFieldToTab("Root.Main", new CheckboxSetField("StaffPages", 'Staff Members Listed On This Page', StaffPage::get()->sort('Title')->map('ID', 'Title')));
 
 		return $f;
